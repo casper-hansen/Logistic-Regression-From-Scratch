715f87d4ff95480acfdc9cbd1b5ea143490c020e
casperbh96 <casperbh.96@gmail.com>
2021-12-22 07:51:34 +0100

    Tune epoch parameter

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from logistic_regression import LogisticRegression as CustomLogisticRegression
 from data import x_train, x_test, y_train, y_test
 
 lr = CustomLogisticRegression()
-lr.fit(x_train, y_train, epochs=100)
+lr.fit(x_train, y_train, epochs=150)
 pred = lr.predict(x_test)
 accuracy = accuracy_score(y_test, pred)
 print(accuracy, pred)
 
-model = LogisticRegression(solver='newton-cg', max_iter=100)
+model = LogisticRegression(solver='newton-cg', max_iter=150)
 model.fit(x_train, y_train)
 pred2 = model.predict(x_test)
 accuracy2 = accuracy_score(y_test, pred2)
